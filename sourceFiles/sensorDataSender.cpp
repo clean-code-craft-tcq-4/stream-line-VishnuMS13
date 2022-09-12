8ce86d68ecf93b741e46c98e7556d7629dca0c23
VishnuMS13 <105206650+VishnuMS13@users.noreply.github.com>
2022-09-12 10:49:50 +0530

    Add files via upload

---
+++ sourceFiles/sensorDataSender.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include "../includes/chargeSensorStatus.hpp"
+#include "../includes/temperatureSensorStatus.hpp"
+#include <vector>
+#include "sensorDataSender.hpp"
+
+using namespace std;
+
+bool writeDataToConsole()
+{
+  bool writeStatus = false;
+  vector<int> currentListInAmps = getCurrentListInAmps(MIN_AMP, MAX_AMP, WRITE_COUNT);
+  vector<int> temperatureListInCelcious = getTemperatureInCelcious(TEMPERATURE_MIN_VALUE, TEMPERATURE_MAX_VALUE, WRITE_COUNT);
+
+  if ((currentListInAmps.size() == WRITE_COUNT) && (temperatureListInCelcious.size() == WRITE_COUNT))
+  {
+    for (int counter = 0; counter < WRITE_COUNT; counter++)
+    {
+      cout << currentListInAmps[counter] << ", " << temperatureListInCelcious[counter] << endl;
+    }
+
+    writeStatus = true;
+  }
+
+  return writeStatus;
+}
