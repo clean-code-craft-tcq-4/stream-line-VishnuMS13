4aacdfa2df37b0af15fe89d328e05bd2d2f6e517
deepasn08 <sn.deepa@in.bosch.com>
2022-10-29 20:05:13 +0900

    TestReceiver1

--- Receiver_tester.c
+++ Receiver_tester.c
@@ -14,8 +14,8 @@ void Test_case1(){
     expectedminvalue = -50;
     obtainedminvalue = minValue(&Temperature[0]);
     obtainedmaxvalue = maxValue(&Temperature[0]);
-    printf("The Minimum value of Temperature is %f \n",&obtainedminvalue);
-    printf("The Maximum value of Temperature is %f \n",&obtainedmaxvalue);
+    printf("The Minimum value of Temperature is %f \n",obtainedminvalue);
+    printf("The Maximum value of Temperature is %f \n",obtainedmaxvalue);
     assert(obtainedminvalue >= expectedminvalue);
     assert(obtainedmaxvalue <= expectedmaxvalue);
 
@@ -25,8 +25,8 @@ void Test_case1(){
     obtainedminvalue = minValue(&Current[0]);
     obtainedmaxvalue = maxValue(&Current[0]);
     printReceivedDataToConsole(Current);
-    printf("The Minimum value of Current is %f \n",&obtainedminvalue);
-    printf("The Maximum value of Current is %f \n",&obtainedmaxvalue);
+    printf("The Minimum value of Current is %f \n",obtainedminvalue);
+    printf("The Maximum value of Current is %f \n",obtainedmaxvalue);
 
     assert(obtainedminvalue >= expectedminvalue);
     assert(obtainedmaxvalue <= expectedmaxvalue);
@@ -39,10 +39,10 @@ void Test_case2(){
     receiveAndProcessSensorData(&Current[0],&Temperature[0]);
     // Get the SMA of Temperature
     float SMATemp = getSimpleMovingAverage(&Temperature[0]);
-    printf("The SMA of Temperature is %f \n",&SMATemp);
+    printf("The SMA of Temperature is %f \n",SMATemp);
     // Get the SMA of Current
     float SMACurrent = getSimpleMovingAverage(&Current[0]);
-    printf("The SMA of Temperature is %f \n",&SMACurrent);
+    printf("The SMA of Temperature is %f \n",SMACurrent);
 }
 
 int main(){
