5532435135d44d2873b27b0fbdd6d69b1db9f350
deepasn08 <sn.deepa@in.bosch.com>
2022-10-29 18:45:46 +0900

    testerupdates

--- Receiver_tester.c
+++ Receiver_tester.c
@@ -1,51 +1,47 @@
 #include "includes/Receiver.h"
 #include <assert.h>
 
-//Test the entries
-
+//Check if min and max values are within limits and display on the console
 void Test_case1(){
-//If the generated values are constant, for random generation the values are changed constantly
-    float Temperature[numberofreading] = {0};
-    float Current[numberofreading]= {0};
-    float expectedData [2][2] = {{-10,-46}, {9,-28}};
-    receiveAndProcessSensorData(&Temperature[0],&Current[0]);
-    for (int i= 0; i<2;i++){
-        assert(Current[i] == expectedData [i][0]);
-        assert(Temperature[i] == expectedData [i][1]);
-        }
-}
-
-//Test Min, MAx and SMA
-void Test_case2(){
     float Temperature[numberofreading] = {0};
     float Current[numberofreading]= {0};
     float obtainedminvalue, obtainedmaxvalue, obtainedSMA, expectedminvalue, expectedmaxvalue, expectedSMA;
     //Temparture
     expectedmaxvalue = 150;
     expectedminvalue = -50;
-    //expectedSMA = 11.6; If the generated values are constant, for random generation the values are changed constantly 
     obtainedminvalue = minValue(&Temperature[0]);
     obtainedmaxvalue = maxValue(&Temperature[0]);
-    //obtainedSMA = getSimpleMovingAverage(&Temperature[0]); If the generated values are constant, for random generation the values are changed constantly 
-
+    printf("The Minimum value of Temperature is %f",&obtainedminvalue);
+    printf("The Maximum value of Temperature is %f",&obtainedmaxvalue);
     assert(obtainedminvalue >= expectedminvalue);
     assert(obtainedmaxvalue <= expectedmaxvalue);
-    //assert(obtainedSMA == expectedSMA); If the generated values are constant, for random generation the values are changed constantly
 
     //Current
     expectedmaxvalue = 15;
     expectedminvalue = -15;
-    //expectedSMA = 5; If the generated values are constant, for random generation the values are changed constantly
     obtainedminvalue = minValue(&Current[0]);
     obtainedmaxvalue = maxValue(&Current[0]);
-    //obtainedSMA = getSimpleMovingAverage(&Current[0]); If the generated values are constant, for random generation the values are changed constantly
+    printf("The Minimum value of Current is %f",&obtainedminvalue);
+    printf("The Maximum value of Current is %f",&obtainedmaxvalue);
 
     assert(obtainedminvalue >= expectedminvalue);
     assert(obtainedmaxvalue <= expectedmaxvalue);
-    //assert(obtainedSMA == expectedSMA); If the generated values are constant, for random generation the values are changed constantly
+}
+
+void Test_case2(){
+    //Print the obtained SMA values of the Sensor Data
+    float Temperature[numberofreading] = {0};
+    float Current[numberofreading]= {0};
+    receiveAndProcessSensorData(&Temperature[0],&Current[0]);
+    // Get the SMA of Temperature
+    float SMATemp = getSimpleMovingAverage(&Temperature[0]);
+    printf("The SMA of Temperature is %f",&SMATemp);
+    // Get the SMA of Current
+    float SMACurrent = getSimpleMovingAverage(&Current[0]);
+    printf("The SMA of Temperature is %f",&SMACurrent);
 }
 
 int main(){
-    //Test_case1();
+    Test_case1();
     Test_case2();
 }
